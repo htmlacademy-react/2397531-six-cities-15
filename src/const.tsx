2385dd64fd36c7a796ce0498ad8826d7c9e6d39d
Vysotskiy96 <tvgradskyj@gmail.com>
2024-02-21 10:16:12 +0300

    Добавляем main-page и компоненты

---
+++ src/const.tsx
@@ -0,0 +1,9 @@
+const CitiesList = ['Paris', 'Cologne', 'Brussels', 'Amsterdam', 'Hamburg', 'Dusseldorf'];
+
+const SortOptions = ['Popular', 'Price: low to high', 'Price: high to low', 'Top rated first'];
+
+const CountPlaces = {
+  CountOfferPlaces: 312
+} as const;
+
+export {CitiesList, CountPlaces, SortOptions};
---
+++ src/pages/main-page/main-page.tsx
@@ -0,0 +1,48 @@
+import Header from '../../components/header/header';
+import CardList from '../../components/cards-list/cards-list';
+import CityList from '../../components/cities-list/cities-list';
+import SortTypes from '../../components/sort-type/sort-type';
+
+type MainProps = {
+  countPlace: number
+}
+
+function MainPage({countPlace}: MainProps): JSX.Element {
+  return(
+    <div className="page page--gray page--main">
+      <Header />
+      <main className="page__main page__main--index">
+        <h1 className="visually-hidden">Cities</h1>
+        <div className="tabs">
+          <section className="locations container">
+            <CityList />
+          </section>
+        </div>
+        <div className="cities">
+          <div className="cities__places-container container">
+            <section className="cities__places places">
+              <h2 className="visually-hidden">Places</h2>
+              <b className="places__found">{countPlace} places to stay in Amsterdam</b>
+              <form className="places__sorting" action="#" method="get">
+                <span className="places__sorting-caption">Sort by </span>
+                <span className="places__sorting-type" tabIndex={0}>
+                  Popular
+                  <svg className="places__sorting-arrow" width={7} height={4}>
+                    <use xlinkHref="#icon-arrow-select" />
+                  </svg>
+                </span>
+                <SortTypes />
+              </form>
+              <CardList />
+            </section>
+            <div className="cities__right-section">
+              <section className="cities__map map" />
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default MainPage;
